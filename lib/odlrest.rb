abe7e1ccf6586909082a2206e3992177d39eaa2f
Gianpietro Lavado <gianpietro1@gmail.com>
2016-06-12 08:31:54 -0500

    production

--- lib/odlrest.rb
+++ lib/odlrest.rb
@@ -4,6 +4,7 @@ class Odlrest
     @odl_server = '10.0.2.2'
   elsif Rails.env.production?
     @odl_server = '172.31.37.109'
+  end
 
   def self.getnodes
     base_url = "http://admin:admin@#{@odl_server}:8181/restconf/operational//network-topology:network-topology/topology/flow:1/"
